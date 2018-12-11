eff194d2fed2c901ec7087dab05af667b683a423
Jeremy <jeremy@jsrath.com>
2018-12-11 21:52:52 +0100

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+//import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'usa-jobs';
+  title;
+  data = {
+    title: 'Money Man',
+    age: 42,
+    gender: 'male',
+  };
+
+  getTitle() {
+    return (this.title = 'USA Jobs');
+  }
 }
