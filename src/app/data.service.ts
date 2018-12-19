ffb69bd32255084aa5ff950fd9b33f9257e8de62
Jeremy <jeremy@jsrath.com>
2018-12-19 22:39:42 +0100

    Updates

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class DataService {
-  constructor() {}
+export class ConfigService {
+  constructor(private http: HttpClient) {}
+  url = 'https://jobs.search.gov/jobs/search.json?query=nursing+jobs+with+veterans+affairs+in+albany+ny';
+  getData() {
+    return this.http.get(this.url);
+  }
 }
